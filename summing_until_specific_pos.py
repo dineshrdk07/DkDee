4bbc25e45763917ddc32141f4db11033354e0613
Dineshrdk07 <43876021+Dineshrdk07@users.noreply.github.com>
2019-07-24 14:58:28 +0530

    Update summing_until_specific_pos.py

--- summing_until_specific_pos.py
+++ summing_until_specific_pos.py
@@ -1,8 +1,6 @@
-import array as a
 num,pos=[int(i) for i in input().split()]
 num_list=[int(i) for i in input()]
 summation=0
-num_arr=a.array('i',num_list)
 for i in range(pos):
     summation=summation+num_arr[i]
 print(summation)
